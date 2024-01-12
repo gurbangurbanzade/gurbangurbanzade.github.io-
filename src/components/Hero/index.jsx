90765685671ae76190e23b16d042ea58b04042d7
gurban gurbanzade <gurban.gurbanzadeh@gmail.com>
2024-01-13 01:53:29 +0400

    button update

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -11,6 +11,7 @@ import Arrow3 from "../../../src/assets/img/heroBannerImg/arrow3.png";
 import Arrow4 from "../../../src/assets/img/heroBannerImg/arrow4.png";
 import Slash from "../../../src/assets/img/heroBannerImg/slash.png";
 import Profile1 from "../../../src/assets/img/heroBannerImg/profile1.png";
+import { FaArrowRight } from "react-icons/fa";
 
 const Hero = ({ onButtonClick }) => {
   return (
@@ -157,19 +158,7 @@ const Hero = ({ onButtonClick }) => {
             <br /> Web and Mobile developer.
           </h1>
           <button className="btn" onClick={onButtonClick} style={{ zIndex: 1 }}>
-            View my work
-            {/* <svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="16"
-              width="16"
-              viewBox="0 0 448 512"
-              className="arrow-left"
-            >
-              <path
-                d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"
-                fill="white"
-              />
-            </svg> */}
+            View my work <FaArrowRight className="arrowLeft" />
           </button>
         </div>
       </div>
